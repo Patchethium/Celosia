ec6ca5908765c59aab9b9cd9a0d8480fa5271636
Patchethium <asguftuikh@gmail.com>
2024-05-27 16:04:29 +0900

    use wer as evaluation metrices

--- train/eval.py
+++ train/eval.py
@@ -1,10 +1,11 @@
 import torch
-from torchtext.data.metrics import bleu_score
+from torcheval.metrics import WordErrorRate
 from train import PhDataset, G2P
 from omegaconf import OmegaConf
 from tqdm.auto import tqdm
 import argparse
 from torch.utils.data import random_split
+
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -13,7 +14,7 @@ def main(lang: str, checkpoint: str):
     alphabets = conf.specials + conf.alphabets
     phonemes = conf.specials + conf.phonemes
     ds = PhDataset(f"./data/{lang}.txt", conf)
-    _train_ds, eval_ds = random_split(ds, [0.99, 0.01])
+    _train_ds, eval_ds = random_split(ds, [0.9, 0.1])
     model = G2P(
         conf.d_model,
         conf.d_special + conf.d_alphabet,
@@ -23,18 +24,16 @@ def main(lang: str, checkpoint: str):
     model.to(DEVICE)
     model.load_state_dict(torch.load(checkpoint, map_location=DEVICE))
     model.eval()
-    predicted = []
-    reference = []
+    wer = WordErrorRate()
     for w, p in tqdm(eval_ds):
         _, ph = model.forward(w.unsqueeze(0), p.unsqueeze(0))
         ph = torch.argmax(ph, dim=-1)
-        predict = [phonemes[int(i)] for i in ph.squeeze()[:-2]]
-        ref = [[phonemes[int(i)] for i in p.squeeze()[1:-1]]]
-        predicted.append(predict)
-        reference.append(ref)
-    print(predicted[0], reference[0])
+        predict = " ".join([phonemes[int(i)] for i in ph.squeeze()[:-2]])
+        ref = " ".join([phonemes[int(i)] for i in p.squeeze()[1:-1]])
+        # print(predict, ref)
+        wer.update([predict], [ref])
 
-    print(f"Bleu Score: {bleu_score(predicted, reference)}")
+    print(f"WER: {wer.compute()}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
